063de87461e33998442c557e12fb18b7b1403709
Ash <maybork@pm.me>
2025-08-19 18:20:42 +0200

    start developing layout

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,18 @@
 <script lang="ts">
+  import Logo from './lib/Logo.svelte';
   import StickerList from './lib/StickerList.svelte';
 </script>
 
-<main>
+<Logo/>
+<main class="layout">
   <StickerList />
+  <div>
+    Randomizer will be here
+  </div>
 </main>
 
 <style>
+  .layout{
+    display: flex;
+  }
 </style>
